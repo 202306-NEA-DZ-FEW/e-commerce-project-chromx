64b97315309e37a15043acf5fa0d6adb44535fd2
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-09-26 23:28:56 +0100

    feat(home page): create banner slide
    create banner slider with animation
    
    fix #21

--- src/components/Banner/BannerText.jsx
+++ src/components/Banner/BannerText.jsx
@@ -1,5 +1,44 @@
-function BannerText() {
-  return <div>BannerText</div>
+import { motion } from "framer-motion"
+import Link from "next/link"
+
+const BannerText = ({ title, subtitle }) => {
+  return (
+    <div className="lg:inline-block absolute top-0 left-0 px-10 w-full h-full">
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black opacity-40"></div>
+      <div className="flex h-full flex-col gap-y-6 justify-center md:ml-10 md:max-w-3xl relative z-20">
+        <motion.h2
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="text-5xl lg:text-6xl font-bold text-white"
+        >
+          {title}
+        </motion.h2>
+        <motion.p
+          initial={{ y: 40, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          className="text-md lg:text-lg text-slate-100"
+        >
+          {subtitle}
+        </motion.p>
+        <motion.div
+          viewport={{ once: true }}
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.7 }}
+          className="flex gap-x-4 mt-2"
+        >
+          <Link href="/products" className="btn btn-neutral">
+            Find out more
+          </Link>
+          <Link href="/products" className="btn btn-success">
+            Shop Now
+          </Link>
+        </motion.div>
+      </div>
+    </div>
+  )
 }
 
 export default BannerText
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,92 @@
-function Banner() {
-  return <div className="h-[500px] bg-red-300">Banner</div>
+import Slider from "react-slick"
+import { PiCaretLeftLight, PiCaretRightLight } from "react-icons/pi"
+import Image from "next/image"
+import BannerText from "./BannerText"
+import { bannerData } from "@/utils/constants"
+
+const Banner = () => {
+  const NextArrow = (props) => {
+    const { onClick } = props
+    return (
+      <button
+        className="btn btn-circle btn-ghost text-2xl z-30 absolute left-2 top-1/2"
+        onClick={onClick}
+      >
+        <PiCaretLeftLight />
+      </button>
+    )
+  }
+  const PrevArrow = (props) => {
+    const { onClick } = props
+    return (
+      <button
+        className="btn btn-circle btn-ghost text-2xl z-30 absolute right-2 top-1/2"
+        onClick={onClick}
+      >
+        <PiCaretRightLight />
+      </button>
+    )
+  }
+  var settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: <PrevArrow />,
+    prevArrow: <NextArrow />,
+  }
+  return (
+    <div className="overflow-hidden relative text-white">
+      <Slider {...settings}>
+        {bannerData.map((banner) => (
+          <div key={banner.id} className="w-full h-[560px] relative">
+            <Image
+              src={banner.imageUrl}
+              alt="bannerone"
+              className="w-full h-full relative object-cover"
+              priority
+              fill
+            />
+            <BannerText title={banner.title} subtitle={banner.description} />
+          </div>
+        ))}
+        {/* <div className="w-full h-[560px] relative">
+          <Image
+            src={banner_1}
+            alt="bannerone"
+            className="w-full h-full relative object-cover"
+            priority
+            fill
+          />
+          <BannerText
+            title="Outware Picks"
+            subtitle="dsajdlasjdlasjd djasldjalsd"
+          />
+        </div> */}
+        {/* <div className="w-full h-[560px] relative">
+          <Image
+            src={banner_2}
+            alt="bannertwo"
+            className="w-full h-full relative object-cover"
+            fill
+          />
+          <BannerText title="Seasonal Offers" />
+        </div>
+        <div className="w-full h-[560px] relative">
+          <Image
+            src={banner_3}
+            alt="bannerthree"
+            className="w-full h-full relative object-cover"
+            fill
+          />
+          <BannerText title="Best for all" />
+        </div> */}
+      </Slider>
+    </div>
+  )
 }
 
 export default Banner
