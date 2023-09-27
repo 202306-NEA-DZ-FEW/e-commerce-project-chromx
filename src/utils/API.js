08d0e6d01c9fe89e735b6ff3f345ce679ed58b06
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-09-27 15:32:24 +0100

    feat(products page): fetch and dsiplay all products
    display all products on products page and add pagination
    
    fix #8

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,17 +5,30 @@ export const fetchAllProducts = async (
   query = "",
   limit = 4,
   skip = 0,
+  price = 0,
+  rating = 0,
 ) => {
   if (category) {
     const response = await fetch(`${BASE_URL}/category/${category}`)
     const data = await response.json()
-    return data
+    //filter the products in the category
+    const filtredProducts = await data.products
+      .filter((p) => p.title.toLowerCase().includes(query.toLowerCase()))
+      .filter((p) => (price ? p.price <= price : true))
+      .filter((p) => (rating ? p.rating >= rating : true))
+
+    return { data, products: filtredProducts }
   } else {
     const response = await fetch(
       `${BASE_URL}/search?q=${query}&limit=${limit}&skip=${skip}`,
     )
     const data = await response.json()
-    return data
+    const filtredProducts = await data.products
+      .filter((p) => (price ? p.price <= price : true))
+      .filter((p) => (rating ? p.rating >= rating : true))
+
+    // const products = await data.products
+    return { data, products: filtredProducts }
   }
 }
 
