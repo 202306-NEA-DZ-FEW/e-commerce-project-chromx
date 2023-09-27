9fcc188f99ad6b32274457433dea2948f9e39f64
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-09-27 12:38:42 +0100

    feat(product card): create product card
    design prduct card to display product infos
    
    fix #23

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,63 @@
-function ProductCard() {
-  return <div></div>
+//library imports
+import Link from "next/link"
+import Image from "next/image"
+import { BiCartAdd } from "react-icons/bi"
+import { Rating } from "react-simple-star-rating"
+
+//component imports
+import FormattedPrice from "./FormattedPrice"
+
+function ProductCard({ product }) {
+  const originalPrice = product.price
+  const discountPercentage = product.discountPercentage
+  const finalPrice = originalPrice - originalPrice * (discountPercentage / 100)
+
+  return (
+    <div className="card card-compact bg-white shadow-sm md:max-w-70 w-60 group">
+      {/* ========== card head ========== */}
+      <Link href={"/products/" + product.id}>
+        <figure className="w-full h-[220px] rounded-2xl relative">
+          <Image
+            fill
+            className="w-full h-full group-hover:scale-110 duration-200 object-cover"
+            src={product.thumbnail}
+            alt={product.title}
+            priority
+          />
+        </figure>
+      </Link>
+      {/* ========== card body ========== */}
+      <div className="card-body">
+        <Link
+          title={product.title}
+          href={"/products/" + product.id}
+          className="card-title"
+        >
+          {product.title.length > 15
+            ? product.title.substring(0, 15) + "..."
+            : product.title}
+        </Link>
+        <Rating
+          initialValue={product.rating}
+          allowFraction
+          readonly
+          SVGstyle={{ display: "inline-block", width: "25px" }}
+        />
+
+        <div className="card-actions justify-between items-center">
+          <h3 className="text-2xl font-bold text-success">
+            <FormattedPrice amount={finalPrice} />
+          </h3>
+          <p className="text-sm line-through inline text-error">
+            <FormattedPrice amount={originalPrice} />
+          </p>
+          <button className="btn btn-sm btn-square btn-ghost">
+            <BiCartAdd className="text-2xl" />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ProductCard
