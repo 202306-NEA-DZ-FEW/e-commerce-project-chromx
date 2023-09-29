535c60f51c55f60b51bd8b9a6a81363b54118764
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-09-29 01:15:47 +0100

    feat(project): add firebase
    add firebase auth, firestore and add to cart function to product card
    
    fix #12

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -3,14 +3,76 @@ import Link from "next/link"
 import Image from "next/image"
 import { BiCartAdd } from "react-icons/bi"
 import { Rating } from "react-simple-star-rating"
+import { auth, db } from "@/utils/firebase"
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  updateDoc,
+  where,
+} from "firebase/firestore"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { toast } from "react-toastify"
 
 //component imports
 import FormattedPrice from "./FormattedPrice"
 
 function ProductCard({ product }) {
+  const [user, loading] = useAuthState(auth)
   const originalPrice = product.price
   const discountPercentage = product.discountPercentage
   const finalPrice = originalPrice - originalPrice * (discountPercentage / 100)
+
+  async function onAddToCart() {
+    if (!user) {
+      toast.error("You need to login first😡", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      })
+      return
+    }
+    const collectionRef = collection(db, "cartItems")
+    const q = query(
+      collectionRef,
+      where("id", "==", product.id),
+      where("user", "==", user.uid),
+    )
+
+    const querySnapshot = await getDocs(q)
+
+    if (!querySnapshot.empty) {
+      const cartItemDoc = querySnapshot.docs[0]
+      const currentQuantity = cartItemDoc.data().quantity
+      await updateDoc(doc(db, "cartItems", cartItemDoc.id), {
+        quantity: currentQuantity + 1,
+        timestamp: serverTimestamp(),
+      })
+      const updatedQuantity = currentQuantity + 1
+      toast.success(
+        `You Added ${updatedQuantity} items of that product so far! 🤑`,
+        {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 1500,
+        },
+      )
+    } else {
+      await addDoc(collectionRef, {
+        ...product,
+        quantity: 1,
+        timestamp: serverTimestamp(),
+        user: user.uid,
+        avatar: user.photoURL,
+        username: user.displayName,
+      })
+      toast.success("Your product has been added successfully 👌", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      })
+    }
+  }
 
   return (
     <div className="card card-compact bg-white shadow-sm md:max-w-70 w-60 group">
@@ -53,7 +115,10 @@ function ProductCard({ product }) {
               <FormattedPrice amount={finalPrice} />
             </h3>
           </div>
-          <button className="btn btn-sm btn-square btn-ghost">
+          <button
+            onClick={onAddToCart}
+            className="btn btn-sm btn-square btn-ghost"
+          >
             <BiCartAdd className="text-2xl" />
           </button>
         </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,13 @@ import "@/styles/globals.css"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import RootLayout from "@/components/layouts/RootLayout"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export default function App({ Component, pageProps }) {
   return (
     <RootLayout>
+      <ToastContainer limit={1} />
       <Component {...pageProps} />
     </RootLayout>
   )
